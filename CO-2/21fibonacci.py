9e76401521166e3923fb131c5c1bba835843c802
SumodR <146819103+SumodR@users.noreply.github.com>
2023-11-01 10:08:22 +0530

    Update 21fibonacci.py

--- CO-2/21fibonacci.py
+++ CO-2/21fibonacci.py
@@ -1,13 +1,15 @@
 f=0
 s=1
 sum=0
+c=0
 n=int(input("Enter limit:"))
-if (n>=0):
-    print("0\t")
-elif (n>=1):
-    print(",1")
-for i in range(2,n+1):
-    sum=f+s
-    print(",",sum)
-    f=s
-    s=sum
+if (n==1):
+    print("0")
+else:
+    print("0")
+    print("1")
+    for i in range(2,n):
+        sum=f+s
+        f=s
+        s=sum
+        print(sum)
