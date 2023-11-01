7dd1da3447e57911ad40d6fc53dda602f7e0a0f1
SumodR <146819103+SumodR@users.noreply.github.com>
2023-11-01 10:37:22 +0530

    Update 24numberpyramid.py

--- CO-2/24numberpyramid.py
+++ CO-2/24numberpyramid.py
@@ -1,12 +1,6 @@
-li=[]
-sum=0
 row=int(input("Enter step number(no.of rows):"))
 print("Enter items:-")
 for i in range(1,row+1):
     for j in range(1,i+1):
         print(i*j,end=" ")
     print("\n")
-    
-
-
-
